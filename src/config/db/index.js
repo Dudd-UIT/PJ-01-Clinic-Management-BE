a14ca2a21925cfce358cbd6595b5d2924de61827
Ngyyen <ngyyenle47@gmail.com>
2024-04-22 11:48:18 +0700

    a

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,7 @@
 const oracledb = require("oracledb");
 
 const dbConfig = {
-  user: "C##QUANLYPHONGKHAM",
+  user: "QUANLYPHONGKHAM",
   password: "Admin123",
   connectString: "localhost/orcl",
 };
