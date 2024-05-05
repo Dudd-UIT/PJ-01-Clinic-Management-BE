789a308d1af6af681daaabb10d0bc3cddf8fc624
Ngyyen <ngyyenle47@gmail.com>
2024-05-05 21:35:14 +0700

    m

--- src/routes/phieukham.js
+++ src/routes/phieukham.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const phieukhamController = require('../app/controllers/PhieuKhamController')
+const phieukhamController = require("../app/controllers/PhieuKhamController");
 
 // /phieukham/...
 
-router.post('/insert-pk', phieukhamController.insertPK)
-router.post('/insert-bn-pk', phieukhamController.insertBNPK)
-router.post('/insert-just-pk', phieukhamController.insertIustPK)
-router.get('/dsdk', phieukhamController.fetchDSDK)
-router.get('/chitiet-pk/:id', phieukhamController.fetchKQKham)
-router.get('/dspk/:id', phieukhamController.fetchPKbyIdHD)
-router.get('/ds-benh/:id', phieukhamController.fetchDSBenh)
+router.post("/insert-pk", phieukhamController.insertPK);
+router.post("/insert-bn-pk", phieukhamController.insertBNPK);
+router.post("/insert-just-pk", phieukhamController.insertIustPK);
+router.get("/dsdk", phieukhamController.fetchDSDK);
+router.get("/chitiet-pk/:id", phieukhamController.fetchKQKham);
+router.get("/dspk/:id", phieukhamController.fetchPKbyIdHD);
+router.get("/ds-benh/:id", phieukhamController.fetchDSBenh);
 
-module.exports = router
+module.exports = router;
